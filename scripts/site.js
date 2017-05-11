395c419d771ac849f3a22b5cafd08c55ce891258
tabbyzwei <t761271435@live.com>
2017-05-11 17:05:31 +0800

    edit

--- scripts/site.js
+++ scripts/site.js
@@ -12,12 +12,12 @@ $(function () {
         var currentItem = $(".nav li.active > a").text();
         if (currentItem != "Home") {
             $(".main-navbar").css("background-color", "rgba(0,0,0,.5)");//"rgba(90,160,245,.8)");
-            $("#narbar_logo_big").attr("src", "http://i4.buimg.com/567571/0dc8b3aff76c8312.png");
-            $("#narbar_logo_small").attr("src", "http://i4.buimg.com/567571/0dc8b3aff76c8312.png");
+            $("#narbar_logo_big").attr("src", "https://www.tuchuang001.com/images/2017/05/11/gas_logo2.png");
+            $("#narbar_logo_small").attr("src", "https://www.tuchuang001.com/images/2017/05/11/gas_logo2.png");
         } else {
             $(".main-navbar").css("background-color", "transparent");
-            $("#narbar_logo_big").attr("src", "http://i2.muimg.com/567571/75cea87e6adec527.png");
-            $("#narbar_logo_small").attr("src", "http://i2.muimg.com/567571/75cea87e6adec527.png");
+            $("#narbar_logo_big").attr("src", "https://www.tuchuang001.com/images/2017/05/11/gas_logo.png");
+            $("#narbar_logo_small").attr("src", "https://www.tuchuang001.com/images/2017/05/11/gas_logo.png");
         }
     })
     $("#btn_Contact_bottom").click(function () {
